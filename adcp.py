add structure function calculation method, fix error with a time shift

adcp.py
@@ -42,6 +42,19 @@ class AdcpData:
                     isdetrended=True
                     )
 
+    def get_structure_function(
+            self,
+            reference_point=1,
+            window="100T",
+            ):
+        reference_point_id = np.argmin(np.abs(self.cells - reference_point))
+        data = pd.DataFrame(
+                index=pd.to_datetime(self.index, unit='s'),
+                columns=np.subtract(self.cells, self.cells[reference_point_id, None]),
+                data=np.subtract(self.currents, self.currents[:, reference_point_id, None]),
+                )
+        return data.rolling(window).mean()
+
 
 class SignatureData:
     def __init__(self, **beams):
@@ -51,9 +64,9 @@ class SignatureData:
 def read_adcp_data(filename, beam):
     with h5py.File(filename) as data_file:
         group = data_file[beam]
-        time_shift = group["timestamps"][:] % 1
+        time_shift = group["timestamps"][0] % 1
         return AdcpData(
                 index=group["timestamps"][:] - time_shift,
                 cells=group["cells"][:],
                 currents=group["currents"][:],
-                ) 
+                )